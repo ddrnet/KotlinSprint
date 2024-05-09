37c3a506875b39e6128f5c3cf7be32263012d11c
Жомов Юрий Андреевич <y.zhomov@bftcom.com>
2024-05-10 01:11:43 +0200

    Update lesson1_task2.kt

--- lesson_1/lesson1_task2.kt
+++ lesson_1/lesson1_task2.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val totalNumberOfOrders: Int = 75
-    val gratitudeMessageForPurchase: String = "Спасибо вам за ваши покупки! Ожидаем вас снова!"
+	val totalNumberOfOrders: Int = 75
+	val gratitudeMessageForPurchase: String = "Спасибо вам за ваши покупки! Ожидаем вас снова!"
 	println(totalNumberOfOrders)
 	println(gratitudeMessageForPurchase)
 	var countOfEmployees: Int = 2000
