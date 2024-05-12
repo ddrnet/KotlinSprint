a0354c19db33feeae85bab5416054061a4221c59
ddrnet <70060157+ddrnet@users.noreply.github.com>
2024-05-12 12:14:31 +0200

    KS-1-5 фикс комментов и формата вывода

--- lesson_1/lesson1_task5.kt
+++ lesson_1/lesson1_task5.kt
@@ -3,11 +3,8 @@ const val SECONDS_IN_MINUTE: Int = 60
 const val MINUTES_IN_HOUR: Int = 60
 
 fun main() {
-    // Перевод секунд в целые часы, минуты и секунды
     val hours: Int = SECONDS_IN_SPACE / SECONDS_IN_MINUTE / MINUTES_IN_HOUR
     val remainingMinutes: Int = (SECONDS_IN_SPACE / SECONDS_IN_MINUTE) % MINUTES_IN_HOUR
     val remainingSeconds: Int = SECONDS_IN_SPACE % SECONDS_IN_MINUTE
-
-    // Вывод времени, проведенного в космосе в формате [часы:минуты:секунды]
-    println("Время, проведенное в космосе: ${"%02d".format(hours)}:${"%02d".format(remainingMinutes)}:${"%02d".format(remainingSeconds)}")
+    println("Время, проведенное в космосе: %02d:%02d:%02d".format(hours, remainingMinutes, remainingSeconds))
 }
