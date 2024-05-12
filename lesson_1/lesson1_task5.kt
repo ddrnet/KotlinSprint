ad333187eee3097fc289bc324a5d74cd2580afc8
ddrnet <70060157+ddrnet@users.noreply.github.com>
2024-05-12 10:55:28 +0200

    KS-1-5 фикс констант

--- lesson_1/lesson1_task5.kt
+++ lesson_1/lesson1_task5.kt
@@ -1,12 +1,12 @@
-fun main() {
-    // Объявление переменной с количеством секунд, которые Гагарин провел в космосе
-    val seconds: Int = 6480
+const val SECONDS_IN_SPACE: Int = 6480
+const val SECONDS_IN_MINUTE: Int = 60
+const val MINUTES_IN_HOUR: Int = 60
 
-    // Перевод секунд в целые минуты и часы
-    val minutes: Int = seconds / 60
-    val hours: Int = minutes / 60
-    val remainingSeconds: Int = seconds % 60
-    val remainingMinutes: Int = minutes % 60
+fun main() {
+    // Перевод секунд в целые часы, минуты и секунды
+    val hours: Int = SECONDS_IN_SPACE / SECONDS_IN_MINUTE / MINUTES_IN_HOUR
+    val remainingMinutes: Int = (SECONDS_IN_SPACE / SECONDS_IN_MINUTE) % MINUTES_IN_HOUR
+    val remainingSeconds: Int = SECONDS_IN_SPACE % SECONDS_IN_MINUTE
 
     // Вывод времени, проведенного в космосе в формате [часы:минуты:секунды]
     println("Время, проведенное в космосе: ${"%02d".format(hours)}:${"%02d".format(remainingMinutes)}:${"%02d".format(remainingSeconds)}")
